7aa0729d86abae23e0a9999c75107926a7f7a0ba
ryanorlando92 <ryan.orlando92@gmail.com>
2022-09-14 13:18:18 -0400

    add basic math funcs

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if(y === 0) {
+        return 'You cannot divide by zero';
+    }
+    return x / y;
+}
